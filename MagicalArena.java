24d67c2e24d8098a9b849633ac0d503289cd86f0
Rahul Vijay <rahulvj2106@gmail.com>
2024-05-03 11:37:36 +0530

    Created Player class using the attributes such as health, strength and attack

---
+++ MagicalArena.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Player {
+    int health;
+    int strength;
+    int attack;
+
+    public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+}
+class MagicalArena{
+    public static void main(String[] args){
+    }
+}
